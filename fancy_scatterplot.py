446b84e0906dcc1135d3a01585dd5ebda81ceef9
Joe D <dasenbrockjw@gmail.com>
2017-11-17 13:59:48 -0600

    checkpoint

--- fancy_scatterplot.py
+++ fancy_scatterplot.py
@@ -1,34 +1,15 @@
+"""Create a scatterplot using plotly
+"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
 from scipy.stats import pearsonr as correl
 
-def create_chart(
-        x, y, names, 
-        out_path=False, anxloc=0, anyloc=0, xy_label=False, title=False, 
-        display_para=False):
+
+def chart(x, y, names, filename,
+          xlab='xlab', ylab='ylab', title='title',
+          annotation_location=(0, 0), display_para=False):
     """Given x, y creates a plot"""
-    # filename for output file
-    filename =''
-    if not out_path:
-        filename = 'test'
-        filename += '.html'
-    else:
-        filename = out_path + '.html'
-
-    # define labels
-    x_label = ''
-    y_label = ''
-
-    if not xy_label:
-        x_label = 'x label'
-        y_label = 'y label'
-    else:
-        x_label = xy_label[0]
-        y_label = xy_label[1]
-
-    if not title:
-        title = 'graph title'
 
     # create fancy scatterplot
     slope, intercept = np.polyfit(x, y, 1)
@@ -47,7 +28,7 @@ def create_chart(
         y=y,
         mode='markers',
         marker=go.Marker(color='#3D3C28'),
-        name=y_label,
+        name=ylab,
         text=names
     )
 
@@ -69,20 +50,20 @@ def create_chart(
         xaxis=dict(
             zerolinecolor='rgb(255,255,255)',
             gridcolor='rgb(255,255,255)',
-            title=x_label
+            title=xlab
         ),
 
         yaxis=dict(
             zerolinecolor='rgb(255,255,255)',
             gridcolor='rgb(255,255,255)',
-            title=y_label
+            title=ylab
         ),
 
         annotations=[
             dict(
-                text='R^2: %s' % round(r2, 2),
-                x=0,
-                y=0,
+                text='R^2: %s slope: %s' % (round(r2, 2), round(slope, 2)),
+                x=annotation_location[0],
+                y=annotation_location[1],
                 showarrow=False
             )
         ]
@@ -92,5 +73,5 @@ def create_chart(
     data = [trace1, trace2]
     fig = go.Figure(data=data, layout=layout)
 
-    # output 
+    # output
     plot(fig, filename=filename)
