eb9bc0b822b8a99f230c98eeb02c53a51c89a382
Joe D <dasenbrockjw@gmail.com>
2017-12-08 15:58:19 -0600

    added tutorial stacked barchart

---
+++ fancy_stacked_barplot.py
@@ -0,0 +1,29 @@
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import numpy as np
+import pandas as pd
+
+
+def chart(in_data, filename):
+
+    trace1 = go.Bar(
+        x=['giraffes', 'orangutans', 'monkeys'],
+        y=[20, 14, 23],
+        name='SF Zoo'
+    )
+    trace2 = go.Bar(
+        x=['giraffes', 'orangutans', 'monkeys'],
+        y=[12, 18, 29],
+        name='LA Zoo'
+    )
+
+    data = [trace1, trace2]
+    layout = go.Layout(
+        barmode='stack'
+    )
+
+    # create figure
+    fig = go.Figure(data=data, layout=layout)
+
+    # output
+    plot(fig, filename=filename)
