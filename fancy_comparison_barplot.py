08b75f2251b5df50df59f99621888cf6e9317d5d
Joe D <dasenbrockjw@gmail.com>
2018-01-19 16:16:35 -0600

    checkpoint

--- fancy_comparison_barplot.py
+++ fancy_comparison_barplot.py
@@ -1,24 +1,3 @@
-"""Creates grouped barplot
-
-For two groups only. Could create another script for more groups. 
-
-Pass in_data. in_data is mean to be a dataframe in this manner:
-
-                       bar1           bar2
-x_category1            3.13          15.84
-x_category2            6.67           6.08
-
-Where in_data index is the x_categories and bar1/bar2 are like data.
-
-In the above example, the 3.13 and 15.84 bars would be grouped 
-together and the 6.67 and 6.08 bars would be grouped together. Bar1
-would be on the left of each bar group.
-
-There can only be two columns in in_data, but there can be unlimited 
-rows, which means that there can be many categories, but only two 
-categorized datasets (i.e. each barroup can only contain two bars).
-
-"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
@@ -32,7 +11,7 @@ def chart(in_data, filename, names1, names2,
             x=list(in_data.index),
             y=in_data[in_data.columns[0]],
             name=in_data.columns[0],
-            text=['n = ' + str(x) for x in names1],
+            text=names1,
             marker=go.Marker(color='#3D3C28')
     )
 
@@ -40,7 +19,7 @@ def chart(in_data, filename, names1, names2,
             x=list(in_data.index),
             y=in_data[in_data.columns[1]],
             name=in_data.columns[1],
-            text=['n = ' + str(x) for x in names2],
+            text=names2,
             marker=go.Marker(color='#9B2D1E')
     )
 
