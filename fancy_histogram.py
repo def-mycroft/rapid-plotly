5d79e1b2d5e6ef6b19327ea6963cbc8c32c3b67b
Joe D <dasenbrockjw@gmail.com>
2018-02-23 12:54:49 -0600

    created functional fancy_histogram - previously was placeholder simple templae

--- fancy_histogram.py
+++ fancy_histogram.py
@@ -1,19 +1,77 @@
-"""Creates a multiple scatterplot
-    17X20M JD - seems to be genearlly applicable. 
-    Need review this and incorporate into chartslib project 
-"""
+"""Function for generic plotly plots"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
 import pandas as pd
 
 
-def chart(in_data, filename):
+def chart(x_values, filename,
+          xlab='xlab', ylab='ylab', title='title', tickvals=None,
+          ticktext=None, text=None,
+          bins={'start': '', 'end': '', 'size': ''},
+          annotation={'text': '', 'xloc': 0, 'yloc': 1}):
+    """Generates a histogram
 
-    data = [go.Histogram(x=in_data)]
+    Note that the brackets are [lower,upper), python style.
+
+    """
+    data = list()
+
+    # if x axis passed, create names else no names
+    names = None
+    hoverinfo = None
+    if bins['start'] != '':
+        names = list()
+        hoverinfo = 'text'
+        for x in range(int(bins['end'] / bins['size'])):
+            names.append(
+                'count between ' + \
+                str(tickvals[x]) + '-' + str(tickvals[x + 1])
+                )
+
+    data.append(go.Histogram(
+        x=x_values,
+        nbinsx=bins,
+        xbins=bins,
+        hoverinfo='y+text',
+        text=names
+    ))
+
+    # create layout
+    layout = go.Layout(
+
+        title=title,
+
+        plot_bgcolor='rgb(229, 229, 229)',
+
+        xaxis=dict(
+            zerolinecolor='rgb(255,255,255)',
+            gridcolor='rgb(255,255,255)',
+            title=xlab,
+            showticklabels=True,
+            tickvals=tickvals,
+            ticktext=ticktext
+        ),
+
+        yaxis=dict(
+            zerolinecolor='rgb(255,255,255)',
+            gridcolor='rgb(255,255,255)',
+            title=ylab
+        ),
+
+        annotations=[
+            dict(
+                text=annotation['text'],
+                x=annotation['xloc'],
+                y=annotation['yloc'],
+                showarrow=False
+            )
+        ]
+
+    )
 
     # create figure
-    fig = go.Figure(data=data)
+    fig = go.Figure(data=data, layout=layout)
 
     # output
     plot(fig, filename=filename)
