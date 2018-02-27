bcce6721de8ea0633d57ec7908e036897845608c
Joe D <dasenbrockjw@gmail.com>
2018-02-27 16:10:59 -0600

    changed data input format and updated multiple boxplot

--- fancy_multiple_boxplot.py
+++ fancy_multiple_boxplot.py
@@ -1,21 +1,35 @@
-"""Creates a multiple boxplot 
-"""
+"""Creates a multiple boxplot"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
 import pandas as pd
 
 
-def chart(in_data, names, filename, 
-        name='name', title='title', ylab='ylab', boxpoints=None):
+def chart(in_data, category_col, data_col, filename,
+          name='name', title='title', ylab='ylab', boxpoints=None):
     """Creates fancy boxplots with plotly.
-
-    Uses same axes for both plots. 
 
     Boxpoints arg set to 'all' shows actual data points beside boxplot
 
+    Example data input:
+
+        sales_rep          percent_from_model
+        Solomon L Grover      -0.0828
+        Justin C Blecha       -0.1051
+        Rebecca J White        0.7145
+        Jordan T Cairns       -0.7353
+        Justin R Hammon       -0.7065
+        Justin C Blecha       -0.4243
+        John L Pugh           -0.2618
+        Daniel B Elmore       -0.2304
+        James Reilly           0.4012
+        Jacob T Bertram       -0.9181
+
+    To plot this data, pass category_col='sales_rep' and
+    data_col='percent_from_model'
+
     """
-    # create box objects 
+    # create box objects
     def create_plot(data_series, name):
         """Creates a boxplot"""
         box = go.Box(
@@ -25,33 +39,32 @@ def chart(in_data, names, filename,
             boxmean=True,
             marker=dict(
                 color='#3D3C28'
-                )
+            )
         )
 
         return box
 
     data = list()
-    for name in names:
-        series = in_data[name]
-        total = '${:,.3f}'.format(series.sum() / 1000000)
-        name = '%s<br>(n=%s, sum=%sM)' % (
-                name, len(series), total
-                )
+    for category in in_data[category_col].unique():
+        series = in_data[in_data[category_col] == category][data_col]
+        name = '%s (n=%s)' % (
+            category, len(series)
+        )
         data.append(create_plot(series, name))
 
     # create layout
     layout = go.Layout(
 
-            title=title,
-            plot_bgcolor='rgb(229, 229, 229)',
-            showlegend=False,
+        title=title,
+        plot_bgcolor='rgb(229, 229, 229)',
+        showlegend=False,
 
-            yaxis=dict(
-                zerolinecolor='rgb(255,255,255)',
-                gridcolor='rgb(255,255,255)',
-                title=ylab
-            )
-            )
+        yaxis=dict(
+            zerolinecolor='rgb(255,255,255)',
+            gridcolor='rgb(255,255,255)',
+            title=ylab
+        )
+    )
 
     # create figure
     fig = go.Figure(data=data, layout=layout)
