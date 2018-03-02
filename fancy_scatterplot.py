824231282ff76cd97ff937c1eacbd5fced3bda36
Joe D <dasenbrockjw@gmail.com>
2018-03-02 14:28:32 -0600

    tweaked scatterplot to have option of suppressing model

--- fancy_scatterplot.py
+++ fancy_scatterplot.py
@@ -8,6 +8,8 @@ TODO - want to be able to show inline in jupyter, create switch
 TODO - see cvpl99020 project. have updated version of this which
 allows for modification of layout by passing dict as arg. 
 
+TODO - allow for custom annotation to be passed
+
 """
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
@@ -18,7 +20,7 @@ from scipy.stats import pearsonr as correl
 def chart(x, y, names, filename,
           xlab='xlab', ylab='ylab', title='title',
           annotation_location=(0, 0), return_model=False, xtick_height=False,
-          ytick_height=False, hoverinfo=None):
+          ytick_height=False, hoverinfo=None, suppress_model=False):
     """Given x, y creates a plot"""
 
     # create fancy scatterplot
@@ -26,7 +28,7 @@ def chart(x, y, names, filename,
     xi = x
     line = slope * xi + intercept
 
-    # define model 
+    # define model
     def model(x, view_coefficients=False):
         """Simple model function"""
         # TODO have hacky way of viewing coefficients...
@@ -34,7 +36,7 @@ def chart(x, y, names, filename,
         if view_coefficients:
             print('slope: %s' % slope)
             print('intercept: %s' % intercept)
-        return slope*x + intercept
+        return slope * x + intercept
 
     # calculate r2 value
     r2 = correl(x, y)[0]**2
@@ -59,19 +61,36 @@ def chart(x, y, names, filename,
         name='Fit'
     )
 
-    # setup y ticks 
+    # setup y ticks
     ydtick = ''
     if ytick_height == False:
         ydtick = max(y) / 20
     else:
         ydtick = ytick_height
 
-    # setup x ticks 
+    # setup x ticks
     xdtick = ''
     if xtick_height == False:
         xdtick = max(x) / 20
     else:
         xdtick = xtick_height
+
+    # setup annotations
+    annotations = [
+        {
+            'text': 'R^2: %s slope: %s' % (round(r2, 2), round(slope, 2)),
+            'x': annotation_location[0],
+            'y':annotation_location[1],
+            'showarrow':False
+        }
+    ]
+
+    # create data list
+    data = [trace1, trace2]
+
+    if suppress_model:
+        annotations = []
+        del data[-1]
 
     # create layout
     layout = go.Layout(
@@ -93,18 +112,10 @@ def chart(x, y, names, filename,
             dtick=ydtick
         ),
 
-        annotations=[
-            dict(
-                text='R^2: %s slope: %s' % (round(r2, 2), round(slope, 2)),
-                x=annotation_location[0],
-                y=annotation_location[1],
-                showarrow=False
-            )
-        ]
+        annotations=annotations
     )
 
-    # create figure
-    data = [trace1, trace2]
+    # create figure 
     fig = go.Figure(data=data, layout=layout)
 
     # output
