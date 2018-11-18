dc7770a739343a863a7163ac5af115f017aa1e82
Joe D <dasenbrockjw@gmail.com>
2018-11-18 14:32:04 -0600

    refactored and wrote docstrings for barplot, updated example nb

--- barplot.py
+++ barplot.py
@@ -1,3 +1,9 @@
+"""Convenience function for creating a Plotly barplot
+
+Use `create_graph` to create an attractive, highly interactive Plotly
+barplot, either in a Jupyter notebook or as an html file. 
+
+"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
@@ -6,93 +12,131 @@ import helpers
 output_graph = helpers.output_graph
 
 
-def create_trace(in_data, colors, col, hoverinfo, names, errors, error_barwidth):
-    """Creats a trace"""
+def create_errors(error, error_barwidth):
+    """Creates dict of errors for barplot"""
 
+    if isinstance(error, str):
+        error_y = {}
+
+    else:
+        error_y = {
+            'type': 'data',
+            'array': error,
+            'thickness': error_barwidth,
+            'width': int((error_barwidth * 2.5) / 2),
+            'visible': True
+        }
+
+    return error_y
+
+
+def create_trace(in_data, colors, col, hoverinfo, names, errors,
+                 error_barwidth):
+    """Creates a barplot trace for a column in `in_data`"""
     if isinstance(errors, str):
         error_y = {}
     else:
-        error_y = helpers.create_errors(errors[col], error_barwidth)
+        error_y = create_errors(errors[col], error_barwidth)
 
     trace = go.Bar(
-            x=list(in_data.index),
-            y=in_data[col],
-            name=col,
-            text=names[col],
-            marker=go.bar.Marker(color=colors[col]),
-            hoverinfo=hoverinfo,
-            error_y=error_y
+        x=list(in_data.index),
+        y=in_data[col],
+        name=col,
+        text=names[col],
+        marker=go.bar.Marker(color=colors[col]),
+        hoverinfo=hoverinfo,
+        error_y=error_y
     )
 
     return trace
 
 
-def create_graph(in_data, filepath='', colors=None, errors='', title='title',
-                 xlab='xlab', ylab='ylab', names='', error_barwidth=7,
-                 hoverinfo=None, annotations=[]):
+def create_graph(in_data, filepath='', names='', errors='',
+                 title='title', xlab='xlab', ylab='ylab', colors='',
+                 error_barwidth=7, layout='', hoverinfo=None, annotations=[]):
     """Creates grouped barplot
 
-    Pass in_data. in_data is mean to be a dataframe in this manner:
+    The `in_data` arg must be a dataframe in the form:
 
                            bar1           bar2
     x_category1            3.13          15.84
     x_category2            6.67           6.08
 
-    Where in_data index is the x_categories and bar1/bar2 are like data.
-
-    Pass "'hoverinfo='text'" to strip the default values from the
-    hovertext.
+    Where `in_data.index` (x_category1, x_category2 above) are the x
+    labels on the graph, and each column (bar1, bar2 in the above
+    example) is a bargroup. Each cell represents the height of the bar.
 
     In the above example, the 3.13 and 15.84 bars would be grouped 
     together and the 6.67 and 6.08 bars would be grouped together. Bar1
     would be on the left of each bar group.
 
-    There isn't an expected limit on the width of `in_data`.
+    Note that `in_data` can be passed with a single column to create
+    a normal barplot (as opposed to a grouped barplot).
 
-    The `names` arg is expected to be a dict where the keys are the 
-    columns of `in_data` and the values are names for each category
-    bar in each of the columns, i.e. a list of length equal to
-    len(in_data.index). 
+    There isn't an expected limit on the number of columns in `in_data`.
+
+    If a filepath is passed, the graph will be written to an html file,
+    otherwise the graph will be displayed in-line (when calling from a
+    Jupyter notebook).
+
+    A dataframe `names` can be passed; a dataframe with the same shape,
+    index and columns as `in_data` where each cell represents the
+    desired hover text for each bar. 
+
+    A dataframe `errors` can be passed; a dataframe with the same shape,
+    index and columns as `in_data` where each cell represents the
+    below and above value for the error bar on each large bar, e.g.
+    if the value is "5", the error bar will range from 5 units above
+    the corresponding bar and 5 units below the corresponding bar. 
+
+    The `error_barwidth` arg represents the width of the error bars in
+    pixels.
+
+    The `title`, `xlab` and `ylab` args are text arguments which 
+    correspond to the main title, x label and y label of the graph. 
 
     The `colors` arg is a dict mapping columns of `in_data` to html
-    colors on the graph
+    colors for each bar. 
 
-    Error bars can be added by passing a dataframe with the same
-    index and columns as `in_data`, where each cell value is the error
-    for the corresponding bar in `in_data`. Each error value is the 
-    "+/-" value for the bar, e.g. if the cell value is "15", then the
-    error bar will be 15 units above and 15 units below the top of 
-    the bar.
+    Pass "'hoverinfo='text'" to strip the default values from the
+    hovertext (and therefore only display the text passed in `names`).
 
-    TODO - create method of editing axis values by keyword args.
+    The annotations arg is expected to be a series of dicts in the form:
 
-    TODO - `colors` and `names` need to be a dataframe instead of a 
-    dict of lists. 
-    
-    TODO - need to be able to pass `colors=''` for default colors, 
-    makes for faster development. 
+        {'text':'annotation text', 'x':10, 'y':15, 'showarrow'=False}
 
+    By default, `plotly` defines the 'x' and 'y' values in terms of the 
+    data on the graph. Annotations have a depth of features in `plotly`,
+    refer to `plotly` documentation for annotation options.
+
+    TODO - create method of editing axis parameters by keyword args.
     """
-    if colors == None:
+    # use default colors if none are passed
+    # otherwise use passed dataframe
+    if isinstance(colors, str):
         colors = helpers.default_colors(in_data.columns)
 
     # setup names and errors if nothing is passed
     if isinstance(names, str):
         names = dict(zip(in_data.columns, in_data.columns))
 
-    # create list of traces 
+    # create list of traces
     data = list()
+
     for col in in_data.columns:
         data.append(create_trace(in_data, colors, col, hoverinfo, names,
                                  errors, error_barwidth))
 
     # create layout
-    helpers.layout['title'] = title
-    helpers.layout['xaxis']['title'] = xlab
-    helpers.layout['yaxis']['title'] = ylab
-    helpers.layout['annotations'] = annotations
+    # if no layout is passed, use default layout from helpers
+    if layout == '':
+        layout = helpers.layout
 
-    layout = go.Layout(helpers.layout)
+    layout['title'] = title
+    layout['xaxis']['title'] = xlab
+    layout['yaxis']['title'] = ylab
+    layout['annotations'] = annotations
+    layout = go.Layout(layout)
 
     # create figure
     fig = go.Figure(data=data, layout=layout)
--- helpers.py
+++ helpers.py
@@ -1,8 +1,10 @@
+"""Misc helper functions used across multiple types of graphs"""
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import numpy as np
 import pandas as pd
 
+# default layout
 layout = {
     'hovermode': 'closest',
     'plot_bgcolor': 'rgb(229, 229, 229)',
@@ -13,38 +15,27 @@ layout = {
               'zerolinecolor': 'rgb(255,255,255)'}
 }
 
+
 def output_graph(filepath, fig):
-    """Given a fig object, generates a graph"""
+    """Given a Plotly fig, generates a graph
+
+    If `filepath` is an empty string, display inline notebook, otherwise
+    write an html file to `filepath`.
+
+    """
     if filepath == '':
         init_notebook_mode(connected=True)
         iplot(fig)
     else:
         plot(fig, filename=filepath, auto_open=False)
 
-def create_errors(error, error_barwidth):
-    """Creates error dict"""
-
-    if isinstance(error, str):
-        error_y = {}
-
-    else:
-        error_y = {
-            'type': 'data',
-            'array': error,
-            'thickness': error_barwidth,
-            'width': int((error_barwidth * 2.5) / 2),
-            'visible': True
-        }
-
-    return error_y
- 
 
 def default_colors(keys, colors=None):
     """Generates a repeating color pallette"""
-    
+
     if colors == None:
         colors = ['#232C65', '#840032', '#E59500', '#00A6FB',
                   '#02040F', '#E4572E']
-    colors = colors * (len(keys)+1)
-    
+    colors = colors * (len(keys) + 1)
+
     return dict(zip(keys, colors[:len(keys)]))
