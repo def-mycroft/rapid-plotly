d9dc7ed5b389ab0b620ec2af852a3416ec0576d6
joe d <dasenbrockjw@gmail.com>
2019-04-27 21:17:16 -0500

    developing the alt feature of lineplot

--- rapid_plotly/lineplot.py
+++ rapid_plotly/lineplot.py
@@ -29,10 +29,11 @@ def create_trace(in_data, colors, col, hoverinfo, names, yaxis=None):
     return trace
 
 
-def create_graph(in_data, filepath='', names='', alt_y=False,
-                 title='title', xlab='xlab', ylab='ylab', y2lab='y2lab', 
-                 colors='', layout='', hoverinfo=None, annotations=[],
-                 aux_traces=[], aux_first=False, hovermode='closest'):
+def create_graph(in_data, filepath='', names='', alt_y=False, title='title',
+                 xlab='xlab', ylab='ylab', y2lab='y2lab', colors='', layout='',
+                 hoverinfo=None, annotations=[], aux_traces=[], aux_first=False,
+                 hovermode='closest', in_data_alt=None, colors_alt='',
+                 names_alt=''):
     """Creates a line plot 
 
     Possible to add lines on alternate axes using `create_trace` and the
@@ -45,28 +46,55 @@ def create_graph(in_data, filepath='', names='', alt_y=False,
     function.
 
     """
+    # setup colors
     # use default colors if none are passed
     # otherwise use passed dataframe
     if isinstance(colors, str):
+        # default colors creates a dictionary where the coloumns
+        # of in_datda are the keys, html color codes are the 
+        # values
         colors = helpers.default_colors(in_data.columns)
 
+    # if there are aux traces and no alt_colors, use reversed
+    # default colors
+    # colors are reversed in this case so that the alt traces have
+    # different colors 
+    if alt_y and isinstance(colors_alt, str):
+        c = in_data_alt.columns.tolist()[::-1]
+        alt_colors = helpers.default_colors(c)
+
+    # setup names
     # setup names and errors if nothing is passed
     if isinstance(names, str):
         names = dict(zip(in_data.columns, in_data.columns))
 
+    # same for alt names 
+    if alt_y and isinstance(names_alt, str):
+        names_alt = dict(zip(in_data_alt.columns, in_data_alt.columns))
+
     # create list of traces
     data = list()
 
-    # if there is a secondary y axis, have to specify this 
-    # when creating the primary trace
-    if alt_y:
-        yaxis='y1' 
-    else:
-        yaxis=None
+    # if there is a secondary y axis, have to specify which axis is 
+    # which, so specify yaxis as 'y1' if there is al alt y
+    # by default `create_trace` requires `yaxis`, set to None if 
+    # only a single axis
+    yaxis = 'y1' if alt_y else None
 
     # create the main traces
     for col in in_data.columns:
-        data.append(create_trace(in_data, colors, col, hoverinfo, names, yaxis))
+        trace = create_trace(in_data, colors, col, hoverinfo, names, yaxis)
+        data.append(trace)
+
+    # create the alt traces
+    if alt_y:
+        alt_traces = []
+        for col in in_data_alt.columns:
+            trace = create_trace(in_data_alt, colors_alt, col, hoverinfo,
+                                 names_alt, yaxis)
+            alt_traces.append(trace)
+
+        data += alt_traces
 
     # if more than one trace, add multiple traces 
     if len(aux_traces) > 0:
